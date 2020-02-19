362e712be29e8cbb9f64d82c0f3f1633aaa38f16
yifan <12941@163.com>
2020-02-20 00:04:21 +0800

    add glusterfs rpms1

---
+++ glusterfs/install.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+
+rpm -ivh attr-2.4.46-13.el7.x86_64.rpm 
+rpm -ivh libaio-0.3.109-13.el7.x86_64.rpm 
+rpm -ivh device-mapper-event-libs-1.02.158-2.el7_7.2.x86_64.rpm 
+rpm -ivh device-mapper-persistent-data-0.8.5-1.el7.x86_64.rpm 
+rpm -ivh psmisc-22.20-16.el7.x86_64.rpm
+rpm -ivh userspace-rcu-0.10.0-3.el7.x86_64.rpm 
+rpm -ivh tcp_wrappers-libs-7.6-77.el7.x86_64.rpm
+rpm -ivh libtirpc-0.2.4-0.16.el7.x86_64.rpm
+rpm -ivh --force systemd-libs-219-67.el7_7.3.x86_64.rpm 
+rpm -ivh --force systemd-219-67.el7_7.3.x86_64.rpm 
+
+
+
+rpm -ivh device-mapper-event-1.02.158-2.el7_7.2.x86_64.rpm 
+
+rpm -ivh device-mapper-libs-1.02.158-2.el7_7.2.x86_64.rpm 
+rpm -ivh --force device-mapper-1.02.158-2.el7_7.2.x86_64.rpm
+
+rpm -ivh glusterfs-api-7.3-1.el7.x86_64.rpm 
+rpm -ivh glusterfs-cli-7.3-1.el7.x86_64.rpm 
+rpm -ivh glusterfs-client-xlators-7.3-1.el7.x86_64.rpm 
+rpm -ivh glusterfs-fuse-7.3-1.el7.x86_64.rpm 
+rpm -ivh glusterfs-libs-7.3-1.el7.x86_64.rpm 
+
+
+rpm -ivh lvm2-2.02.185-2.el7_7.2.x86_64.rpm 
+rpm -ivh rpcbind-0.2.0-48.el7.x86_64.rpm 
+rpm -ivh lvm2-libs-2.02.185-2.el7_7.2.x86_64.rpm 
+#rpm -ivh --force systemd-libs-219-67.el7_7.3.x86_64.rpm 
+rpm -ivh systemd-sysv-219-67.el7_7.3.x86_64.rpm 
+#rpm -ivh tcp_wrappers-libs-7.6-77.el7.x86_64.rpm 
+
+
+#rpm -ivh --force systemd-219-67.el7_7.3.x86_64.rpm 
+
+
+rpm -ivh glusterfs-server-7.3-1.el7.x86_64.rpm 
+
